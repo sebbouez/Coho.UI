6bb782b1915ffbdf5712af0c8f7212592a92cb11
Sébastien Bouez <39953434+sebbouez@users.noreply.github.com>
2023-08-21 19:03:21 +0200

    Fix invisible menus in windows 10

--- Coho.UI/Controls/Menus/AcrylicSubMenu.cs
+++ Coho.UI/Controls/Menus/AcrylicSubMenu.cs
@@ -15,8 +15,11 @@
 
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
 
 namespace Coho.UI.Controls.Menus;
 
@@ -34,6 +37,43 @@ public class AcrylicSubMenu : Popup
                 AcrylicHelper.SetBorder(hwnd.Handle);
                 AcrylicHelper.SetBorderColor(hwnd.Handle);
             }
+        }
+        else
+        {
+            if (Child is Border checkBdr && !string.IsNullOrEmpty(checkBdr.Name) && checkBdr.Name == "LegacyPopupContainerBorder")
+            {
+                return;
+            }
+            
+            UIElement? oldChild = Child;
+            Child = null;
+
+            Border bdr = new()
+            {
+                Name = "LegacyPopupContainerBorder",
+                Margin = new Thickness(14, 0, 14, 14),
+                Effect = new DropShadowEffect()
+                {
+                    Color = (Color) FindResource("ShadowColor"),
+                    Opacity = 0.6, 
+                    Direction = -90,
+                    BlurRadius = 20,
+                    ShadowDepth = 4
+                }
+            };
+
+            Border innerBdr = new()
+            {
+                BorderThickness = new Thickness(1),
+                BorderBrush = (Brush) FindResource("WindowBorder"),
+                MinWidth = 200,
+                CornerRadius = new CornerRadius(8),
+                Background = (Brush) FindResource("MenuBackground")
+            };
+            bdr.Child = innerBdr;
+            innerBdr.Child = oldChild;
+
+            Child = bdr;
         }
     }
 }
